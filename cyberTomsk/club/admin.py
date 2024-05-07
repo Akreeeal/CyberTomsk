d9be506de683836624e9672879adbe363f468ed1
Akreeeal <akreeeal@gmail.com>
2024-05-07 22:59:51 +0700

    add model Computer and migrated it.

---
+++ cyberTomsk/club/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Computer, Reservation
+
+admin.site.register(Computer)
+admin.site.register(Reservation)
---
+++ cyberTomsk/club/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.conf import settings
+class Computer(models.Model):
+    PC_CATEGORIES=(
+        ('VIP', 'VIP'),
+        ('NVIP', 'NON_VIP'),
+        ('BC', 'BOOT_CAMP')
+    )
+    category = models.CharField(max_length=4, choices=PC_CATEGORIES)
+    processor = models.CharField(max_length=255)
+    ram = models.CharField(max_length=255)
+    graphics_card = models.CharField(max_length=255)
+    vram = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f'{self.category} PC with {self.processor} and {self.graphics_card}'
+
+class Reservation(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    computer = models.ForeignKey(Computer, on_delete=models.CASCADE)
+    start_session = models.DateTimeField()
+    stop_session = models.DateTimeField()
+
+    def __str__(self):
+        return f'{self.user} has reserved {self.computer} from {self.start_session} to {self.stop_session}'
